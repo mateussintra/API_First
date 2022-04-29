5f73be9c3a0abe01d40b9642dd7ae2a11b5bd323
Mateus Sintra <mateus.sintra@hotmail.com>
2022-04-29 01:51:29 -0300

    Project part 1 complete

---
+++ Data/_DbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_FF6.Models
+{
+    public class _DbContext : DbContext
+    {
+        public _DbContext(DbContextOptions<_DbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Clients> Clients { get; set; }
+    }   
+}
---
+++ Models/Clients.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_FF6.Models
+{
+    public class Clients
+    {
+        
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]       
+        public string Name { get; set; }
+        
+        public DateTime BirthDate { get; set; }
+        
+        [NotMapped]
+        public int Age { get; set; }
+
+    }
+}
